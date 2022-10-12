4753f7f66f8adf354ca71cb1f55463e090a367b1
BoredLudleth <111277087+BoredLudleth@users.noreply.github.com>
2022-10-12 16:22:17 +0300

    Barabamba

--- NewOnegin.cpp
+++ NewOnegin.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <cstring>
-#include <Windows.h>
+#include <string.h>
+#include <stdlib.h>
 
-void TextRead(FILE *text, FILE *sorted_text, char* allText, char* LinedText, char** index);
+void TextRead(FILE *text, char* allText, char** index, int line, int length);
 
 void TextSorter(FILE *text, FILE *sorted_text);
 
@@ -10,52 +10,48 @@ int lenFile(FILE *text);
 
 int linesFile(FILE *text);
 
-int main(){
-    FILE *text;
-    FILE *sorted_text;
-    text = fopen("TEXT.txt", "r");
-    sorted_text = fopen("SORTEDTEXT.txt", "w");
-    if (text == NULL || sorted_text == NULL) {
+int main()
+{
+    FILE* text = fopen("TEXT3.txt", "rb");
+    FILE*  sorted_text = fopen("SORTEDTEXT.txt", "wb");
+    if (text == NULL) {
         printf("File didn't found");    
     }
 
-    char* allText = (char*) calloc (lenFile(text), sizeof(char));
-    int line = linesFile(text);
-    char* LinedText[line] = {};
-    char* index[line] = {};
-
-    TextRead(text, sorted_text, (char*) allText, (char*) LinedText, (char**) index);
-
-    TextSorter(text, sorted_text);
-
-    fclose(text);
-    fclose(sorted_text);
-}
-
-void TextRead(FILE *text, FILE *sorted_text, char* allText, char* LinedText, char** index)
-{
     int length = lenFile(text);
     int line = linesFile(text);
-    fread(allText, sizeof(char), (size_t) length, text);
-    char* StringText = (char*) calloc(length + line, sizeof(char));
-    *index = StringText;
-    for (int i = 0, count = 0, line = 1; i < length; i++, count++) 
+
+    char* allText = (char*) calloc (length, sizeof(char));
+    char** index = (char**) calloc (line, sizeof(char*));
+    TextRead(text, allText, index, line, length);
+
+    for (int i = 0; i < line; i++)
     {
-        if (*(allText+i) != '\n')
+        fputs(*(index + i), sorted_text);
+        fputs("\n", sorted_text);
+    }
+    fclose(text);
+    fclose(sorted_text);
+
+}
+
+void TextRead(FILE *text, char* allText, char** index, int line, int length)
+{
+    fread(allText, sizeof(char), (size_t) (length + 1), text);
+    int count = 0;
+    *index = allText;
+    for (int i = 0; i < length; i++)
+    {
+        if (allText[i] == '\n') 
         {
-            StringText[count] = allText[i];
-        } else {
-            StringText[count] = '\n';
-            StringText[(count++)] = '\0';
-            *(index + line) = &StringText[(count++)];
-            line++;
+            allText[i] = '\0';
+            count++;
+        }
+        if ((i > 0) && (allText[i - 1] == '\0')) 
+        {
+            *(index + count) = allText + i;
         }
     }
-
-    for (int free_line = 0; free_line < line; free_line++){
-        fputs((const char*) *(index + free_line), sorted_text);
-    }
-    free(StringText);
 }
 
 void TextSorter(FILE *text, FILE *sorted_text) 
@@ -65,7 +61,7 @@ void TextSorter(FILE *text, FILE *sorted_text)
 
 int lenFile(FILE *text){
     fseek(text, 0, SEEK_END);
-    int length = ftell(text);
+    int length =  ftell(text);
     fseek(text, 0, SEEK_SET);
     return length;    
 }
@@ -77,5 +73,6 @@ int linesFile(FILE *text){
         if (ch == '\n')
             lines++;
     }
-    return lines;
+    fseek(text, 0, SEEK_SET);
+    return ++lines;
 }
